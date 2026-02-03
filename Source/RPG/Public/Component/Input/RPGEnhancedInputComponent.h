2e592b62f95b1d88d17dce07f50ab42b0476a896
kailits <2662738608@qq.com>
2026-02-03 15:44:57 +0800

    一些初始文件

---
+++ Source/RPG/Public/Component/Input/RPGEnhancedInputComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "RPGEnhancedInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API URPGEnhancedInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
